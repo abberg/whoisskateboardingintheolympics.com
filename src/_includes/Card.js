70dd0d5ec815add580021b547a2e1077c325f7af
abberg <andrewbberg@gmail.com>
2019-11-28 14:30:16 -0500

    added skater thumbnails for top 5 skaters

--- src/_includes/Card.js
+++ src/_includes/Card.js
@@ -1,10 +1,18 @@
 module.exports = data => /*html*/`
   <div class="card">
-    <p class="card-name">${data.name}</p>
-    <p class="card-country">
-      <span class="flag-icon flag-icon-${data.alpha2.toLowerCase()}"></span>
-      &nbsp;
-      ${data.country}
-    </p>
+    <div class="card-figure">
+      <div 
+        class="card-image"
+        style="background-image:url('img/${data.name.toLowerCase().replace(/ /g, '_')}.jpg')"
+      ></div>
+    </div>
+    <div class="card-content">
+      <p class="card-name">${data.name}</p>
+      <p class="card-country">
+        <span class="flag-icon flag-icon-${data.alpha2.toLowerCase()}"></span>
+        &nbsp;
+        ${data.country}
+      </p>
+    </div>
   </div>
 `;
