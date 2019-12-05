6ae0fbb40c0ed1502a69082ef5d2396bf1232e18
abberg <andrewbberg@gmail.com>
2019-12-05 10:00:54 -0500

    refactored styles to be responsive

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -12,16 +12,22 @@ module.exports = function({rankings, countries}){
     <!DOCTYPE html>
     <html>
       <head>
-        <meta charset="utf-8" />
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Who is Skateboarding in the Olympics?</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:600|Barlow:400,400i,600&display=swap">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/css/flag-icon.min.css">
         <link rel="stylesheet" href="styles.css">
       </head>
-      <body>
-        <header>
+      <body class="wrapper">
+        <header class="page-header">
           <h1>
-            <img src="img/skateboarding-pictogram.svg" alt="Olympic Skateboarding pictogram" width="128"> 
+            <img 
+              src="img/skateboarding-pictogram.svg" 
+              alt="Olympic Skateboarding pictogram" 
+              class="pictogram"
+            /> 
             Who is Skateboarding in the Olympics?
           </h1>
           <p>
@@ -29,53 +35,81 @@ module.exports = function({rankings, countries}){
             <a href="http://www.worldskate.org/olympic-qualifying-system/qualification-system.html">qualification system</a>.
           </p>
         </header>
-        <main>
-          <article>
-            <h2>Street</h2>
-            <div class="event">
-              <section>
-                <h3>Men's Street</h3>
-                <ol>
-                  ${mensStreet
-                    .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
-                    .reduce( (acc, cur) => acc.concat(cur) )
-                  }
-                </ol>
-              </section>
-              <section>
-                <h3>Women's Street</h3>
-                <ol>
-                  ${womensStreet
-                    .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
-                    .reduce( (acc, cur) => acc.concat(cur) )
-                  }
-                </ol>
-              </section>
+        <main class="main-content">
+          <section class="standings">
+            <div class="discipline">
+              <h2 class="discipline-heading">Street</h2>
+              <div class="events">
+                <div class="event event-one">
+                  <h3 class="event-heading">Men's Street</h3>
+                  <ol class="competitor-list">
+                    ${
+                      mensStreet
+                        .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
+                        .reduce( (acc, cur) => acc.concat(cur) )
+                    }
+                  </ol>
+                </div>
+                <div class="event event-two">
+                  <h3 class="event-heading">Women's Street</h3>
+                  <ol class="competitor-list">
+                    ${
+                      womensStreet
+                        .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
+                        .reduce( (acc, cur) => acc.concat(cur) )
+                    }
+                  </ol>
+                </div>
+              </div>
             </div>
-          </article>
-          <article>
-            <h2>Park</h2>
-            <div class="event">
-              <section>
-                <h3>Men's Park</h3>
-                <ol>
-                  ${mensPark
-                    .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
-                    .reduce( (acc, cur) => acc.concat(cur) )
-                  }
-                </ol>
-              </section>
-              <section>
-                <h3>Women's Park</h3>
-                <ol>
-                  ${womensPark
-                    .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
-                    .reduce( (acc, cur) => acc.concat(cur) )
-                  }
-                </ol>
-              </section>
+            <div class="discipline">
+              <h2 class="discipline-heading">Park</h2>
+              <div class="events">
+                <div class="event event-one">
+                  <h3 class="event-heading">Men's Park</h3>
+                  <ol class="competitor-list">
+                    ${
+                      mensPark
+                        .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
+                        .reduce( (acc, cur) => acc.concat(cur) )
+                    }
+                  </ol>
+                </div>
+                <div class="event event-two">
+                  <h3 class="event-heading">Women's Park</h3>
+                  <ol class="competitor-list">
+                    ${
+                      womensPark
+                        .map( skater => `<li>${Card({...skater, alpha2: getCountryAlpha2(skater)})}</li>` )
+                        .reduce( (acc, cur) => acc.concat(cur) )
+                    }
+                  </ol>
+                </div>
+              </div>
             </div>
-          </article>
+          </section>
+          <section>
+            <h2>How does this work?</h2>
+            <p>
+              Each of the four events takes the top 20 skaters by points ranking.<br />
+              <strong>But&hellip;</strong><br />
+              Each country has a maximum of three skaters in each event.<br />
+              <strong>And&hellip;</strong><br />
+              Each continental region must have a competitor.<br />
+              <strong>Also&hellip;</strong><br />
+              Japan must be represented in each event.<br />
+              <strong>Oh, and one more thing&hellip;</strong><br />
+              The top three places in the 2020 World Championships for 
+              <a 
+                href="http://www.worldskate.org/skateboarding/events-skateboarding/competitions/event/73.html" 
+                title="World Skate Park World Championships"
+              >Park</a> and 
+              <a 
+                href="http://www.worldskate.org/skateboarding/events-skateboarding/competitions/event/72.html" 
+                title="World Skate / SLS World Championships"
+              >Street</a> automatically qualify.
+            </p>
+          </section>
         </main>
       </body>
     </html>
